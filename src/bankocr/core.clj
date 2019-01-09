e4b88fd5cd24a0aaf85fbf039d7b8e852417a244
Jason Stewart <jason.m.stewart@gmail.com>
2019-01-09 15:39:11 -0500

    Set up specs for cell->number

--- src/bankocr/core.clj
+++ src/bankocr/core.clj
@@ -1,7 +1,20 @@
-(ns bankocr.core)
+(ns bankocr.core
+  (:require [clojure.spec.alpha :as s]))
+
+(s/def ::cell-row
+  (s/coll-of char? :count 3))
+
+(s/def ::cell
+  (s/coll-of ::cell-row :count 3))
 
 (defn cell->number
   "reads a number from a 3x3 cell"
   [cell]
   ;; For now we just fail the test
   false)
+
+(s/fdef cell->number
+  :args ::cell
+  :ret number?
+  :fn #(and (>= % 0)
+            (<= % 9)))
