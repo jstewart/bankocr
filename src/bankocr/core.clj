6717b8ae0ed46181c01fab7d4ab28bcd4c0f218d
Jason Stewart <jason.m.stewart@gmail.com>
2019-01-09 19:52:22 -0500

    Finish tests for numeric representation detection

--- src/bankocr/core.clj
+++ src/bankocr/core.clj
@@ -8,10 +8,19 @@
   (s/coll-of ::cell-row :count 3))
 
 (defn cell->number
-  "reads a number from a 3x3 cell"
+  "identifies a number from a 3x3 cell of characters"
   [cell]
-  ;; For now we just fail the test
-  false)
+  (condp = cell
+    '((\space \_ \space) (\| \space \|) (\| \_ \|))                 0
+    '((\space \space \space) (\space \space \|) (\space \space \|)) 1
+    '((\space \_ \space) (\space \_ \|) (\| \_ \space))             2
+    '((\space \_ \space) (\space \_ \|) (\space \_ \|))             3
+    '((\space \space \space) (\| \_ \|) (\space \space \|))         4
+    '((\space \_ \space) (\| \_ \space) (\space \_ \|))             5
+    '((\space \_ \space) (\| \_ \space) (\| \_ \|))                 6
+    '((\space \_ \space) (\space \space \|) (\space \space \|))     7
+    '((\space \_ \space) (\| \_ \|) (\| \_ \|))                     8
+    '((\space \_ \space) (\| \_ \|) (\space \_ \|))                 9))
 
 (s/fdef cell->number
   :args ::cell
--- test/bankocr/core_test.clj
+++ test/bankocr/core_test.clj
@@ -2,13 +2,94 @@
   (:require [clojure.test :refer [deftest testing is are]]
             [bankocr.core :refer [cell->number]]))
 
+(defn textual-to-cell
+  "turns a `textual` representation of a digit into a 3x3 cell"
+  [textual]
+  (->> textual
+       seq
+       (partition 3)))
+
+;; Textual representations of numbers (OCR cell)
+;; Attempts made to line the strings up may have the best results
+;; by adjusting to a monospaced font
+(def zero  (str " _ "
+                "| |"
+                "|_|"))
+
+(def one   (str "   "
+                "  |"
+                "  |"))
+
+(def two   (str " _ "
+                " _|"
+                "|_ "))
+
+(def three (str " _ "
+                " _|"
+                " _|"))
+
+(def four  (str "   "
+                "|_|"
+                "  |"))
+
+(def five  (str " _ "
+                "|_ "
+                " _|"))
+
+(def six   (str " _ "
+                "|_ "
+                "|_|"))
+
+(def seven (str " _ "
+                "  |"
+                "  |"))
+
+(def eight (str " _ "
+                "|_|"
+                "|_|"))
+
+(def nine  (str " _ "
+                "|_|"
+                " _|"))
+
 ;; Tests to ensure that numbers 0-9 are correctly identified
 (deftest cell->number_test
   (testing "detecting a zero"
-    (let [textual-zero (str " _ "
-                            "| |"
-                            "|_|")
-          cell (->> textual-zero
-                    seq
-                    (partition 3))]
-      (is (= 0 (cell->number cell))))))
+    (is (= 0 (cell->number
+              (textual-to-cell zero)))))
+
+  (testing "detecting a one"
+    (is (= 1 (cell->number
+              (textual-to-cell one)))))
+
+  (testing "detecting a two"
+    (is (= 2 (cell->number
+              (textual-to-cell two)))))
+
+  (testing "detecting a three"
+    (is (= 3 (cell->number
+              (textual-to-cell three)))))
+
+  (testing "detecting a four"
+    (is (= 4 (cell->number
+              (textual-to-cell four)))))
+
+  (testing "detecting a five"
+    (is (= 5 (cell->number
+              (textual-to-cell five)))))
+
+  (testing "detecting a six"
+    (is (= 6 (cell->number
+              (textual-to-cell six)))))
+
+  (testing "detecting a seven"
+    (is (= 7 (cell->number
+              (textual-to-cell seven)))))
+
+  (testing "detecting a eight"
+    (is (= 8 (cell->number
+              (textual-to-cell eight)))))
+
+  (testing "detecting a nine"
+    (is (= 9 (cell->number
+              (textual-to-cell nine))))))
